45b639e7bf9a5bf377b19975e41646d4b53f905d
SohelShahid117 <sohelshahid09@gmail.com>
2024-02-26 06:36:49 +0600

    32-5 7 min teke soro hbe

--- double-triple.js
+++ double-triple.js
@@ -1,8 +1,14 @@
 // 32-4 Double Equal (==) Vs Triple Equal (===), Implicit Conversion
 
 const a = 1;
-const b = 1;
-if (a == b) {
-    console.log('bothe r  ')
+// const b = '1';
+const b = true;
+if (a === b) {
+    console.log('bothe r same ')
+}else{
+    console.log('bothe r not same ')
 }
-//4 min teke 32-4 soro hobe 
+//4 min teke 32-4 soro hobe
+//type casting or type conversion in js 
+//https://www.geeksforgeeks.org/primitive-and-non-primitive-data-types-in-javascript/
+// https://youtu.be/tLo6g3yEm7o?si=KaoMW3YbY0QY7uko
---
+++ scpe.js
@@ -0,0 +1,13 @@
+function add(a,b){
+    // const first = a;
+    // const second = b;
+    // const total = first + second;
+    const total = a+b;
+    if(a>b){
+        const sub = a-b;
+        console.log(sub)
+    }
+    return total;
+}
+// add(10,5);
+console.log(add(10,5));
